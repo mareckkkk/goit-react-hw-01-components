fe580d9ad28a5c7ec735b1d1a5dfd016e99045f5
mareckkkk <m.walus94@gmail.com>
2024-06-03 13:14:11 +0200

    vite config

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/goit-react-hw-01-components/",
+  base: "/GOIT-REACT-HW-01-COMPONENTS/",
   build: {
     outDir: "./dist",
   },
